8e8804a7764eeff48d209bc0673f15ada1c4795d
Gabriel Antunes <gabrielantunesemp@gmail.com>
2023-11-28 01:17:36 -0300

    Novo Portifílio Layout

--- modal-encaminhamento/modal.js
+++ modal-encaminhamento/modal.js
@@ -1,13 +1,38 @@
 // Função para abrir o modal
-function openModal() {
-    var modal = document.getElementById('myModal');
+function myModalLinkedin() {
+    var modal = document.getElementById('myModalLinkedin');
     modal.style.display = 'flex';
     modal.classList.add('modal-open'); // Adiciona a classe para aplicar a transição
 }
 
+function myModalSocial() {
+    var modal = document.getElementById('myModalSocial');
+    modal.style.display = 'flex';
+    modal.classList.add('modal-open'); // Adiciona a classe para aplicar a transição
+}
+
+function myModalContato() {
+    var modal = document.getElementById('myModalContato');
+    modal.style.display = 'flex';
+    modal.classList.add('modal-open'); // Adiciona a classe para aplicar a transição
+}
+
+
 // Função para fechar o modal
-function closeModal() {
-    var modal = document.getElementById('myModal');
+function closeModalLinkedin() {
+    var modal = document.getElementById('myModalLinkedin');
+    modal.style.display = 'none';
+    modal.classList.remove('modal-open'); // Remove a classe para aplicar a transição
+}
+
+function closeModalSocial() {
+    var modal = document.getElementById('myModalSocial');
+    modal.style.display = 'none';
+    modal.classList.remove('modal-open'); // Remove a classe para aplicar a transição
+}
+
+function closeModalContato() {
+    var modal = document.getElementById('myModalContato');
     modal.style.display = 'none';
     modal.classList.remove('modal-open'); // Remove a classe para aplicar a transição
 }
